5ada6918e82dfb1b37e2977520a756b5d32b66c8
Lufie629 <153257488+Lufie629@users.noreply.github.com>
2023-12-09 14:32:36 +0800

    Update Init.sh

--- Init.sh
+++ Init.sh
@@ -2,8 +2,8 @@
 # step1: prepare the enviroment
 pip install -r requriments.txt
 
-# step2: get the datasets, and you can alse download them from the url:https://drive.google.com/file/d/1T_WzrscrP1tzDVBDYFv6K7HAVT9FkJ7x/view?usp=drive_link
-gdown https://drive.google.com/uc?id=1T_WzrscrP1tzDVBDYFv6K7HAVT9FkJ7x
+# step2: get the datasets, and you can alse download them from the url:https://drive.google.com/file/d/1_NFdFUKpXdLbcsb1e49fNAgKG9EzyT4a/view?usp=drive_link
+gdown https://drive.google.com/uc?id=1_NFdFUKpXdLbcsb1e49fNAgKG9EzyT4a
 
 # step3: The compressed file name is 'Datasets_process.tar.gz'. After decompression, you will obtain the 'Datasets_process' folder. The directory structure is as shown above.
 tar -xzvf Datasets_process.tar.gz
